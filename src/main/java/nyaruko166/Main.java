47dcaadba2bb6e5f279fd53e30aeb0a84023b1e3
Nyaruko166 <occho1666@gmail.com>
2023-06-27 20:19:58 +0700

    Done SettingView

--- src/main/java/nyaruko166/Main.java
+++ src/main/java/nyaruko166/Main.java
@@ -1,64 +1,60 @@
 package nyaruko166;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import lombok.val;
+import org.apache.commons.io.FileUtils;
+
+import javax.swing.*;
+
+import static java.awt.Desktop.getDesktop;
+import static java.util.Arrays.asList;
 
 public class Main {
+
+    
+
     public static void main(String[] args) {
+
+//        String steamInstallationPath = "C:\\Program Files (x86)\\Steam\\steam.exe";
+//        startGameSteam(steamInstallationPath);
         String pathSteam = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Deep Rock Galactic\\FSD\\Saved";
         String pathCrack = "D:\\Games\\Deep.Rock.Galactic_LinkNeverDie.Com\\FSD\\Saved";
-        String fileExtention = ".sav";
-        File steamFile = getFileSteam(pathSteam, fileExtention);
-        File crackFile = getFileSteam(pathCrack, fileExtention);
-        if (steamFile != null && crackFile != null) {
-            Date dateSteam = new Date(steamFile.lastModified());
-            Date dateCrack = new Date(crackFile.lastModified());
-            int result = dateSteam.compareTo(dateCrack);
-            if (result<0){
-                System.out.println("Steam cũ hơn Crack");
-            } else if (result>0) {
-                System.out.println("Steam mới hơn Crack");
-            }else {
-                System.out.println("Steam == Crack");
-            }
-        }
+        
     }
 
-    public static File getFileSteam(String path, String fileExtension) {
-        File folderSteam = new File(path + "\\SaveGames");
-        if (folderSteam.exists() && folderSteam.isDirectory()) {
-            File lastestFile = null;
+    public void copyDir(Path src, Path dest) {
+        String destStr = dest.toString();
 
-            Long lastestModifiedTime = Long.MIN_VALUE;
-
-            File[] lstFile = folderSteam.listFiles();
-
-            if (lstFile != null) {
-                for (File x : lstFile) {
-                    if (x.isFile() && x.getName().endsWith(fileExtension)) {
-                        if (!x.getName().contains("Backup") && !x.getName().contains("ExternalBackup")) {
-                            Long modifiedTime = x.lastModified();
-                            if (modifiedTime > lastestModifiedTime) {
-                                lastestModifiedTime = modifiedTime;
-                                lastestFile = x;
-                            }
-                        }
-
-                    }
-                }
-            }
-//            if (lastestFile != null) {
-//                System.out.println("File có ngày chỉnh sửa gần nhất là: " + lastestFile.getAbsolutePath());
-//                System.out.println("Ngày chỉnh sửa gần nhất: " + lastestFile.lastModified());
-//            } else {
-//                System.out.println("Không tìm thấy file có đuôi " + fileExtension + " trong thư mục.");
-//            }
-//        } else {
-//            System.out.println("Thư mục không tồn tại hoặc không hợp lệ.");
-//
-            //}
-            return lastestFile;
+        try {
+            FileUtils.deleteDirectory(new File(destStr));
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
+
+        new File(destStr).mkdirs();
+
+        try {
+            Files.walk(src).forEach(s -> {
+                try {
+                    Files.copy(s, dest.resolve(src.relativize(s)), StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
